5e47686a2f908dad64b4cd21facaf241e4b68ecb
Itsarun01 <hey.arunkumar05@gmail.com>
2025-02-26 01:19:23 +0530

    add animation on name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,27 @@
 /* eslint-disable react/no-unescaped-entities */
 import {Github, Linkedin, Mail, X} from "lucide-react";
+import {motion} from "framer-motion";
 
 const About = () => {
+  const text = "Arun Kumar".split("");
   return (
     <div>
       <header className="max-w-4xl mx-auto px-6 py-12 ">
-        <h1 className="text-4xl font-bold">Arun Kumar</h1>
+        <h1 className="text-4xl font-bold">
+          {text.map((el, i) => (
+            <motion.span
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              transition={{
+                duration: 0.25,
+                delay: i / 5,
+              }}
+              key={i}
+            >
+              {el}
+            </motion.span>
+          ))}
+        </h1>
         <p className="mt-4 text-xl text-[#d6d6d6]">
           <strong>Web dev & Freelancer</strong>
         </p>
@@ -21,17 +37,26 @@ const About = () => {
           to network, feel free to reach out. Let's create something amazing
           together.
         </p>
-        <div className="flex gap-4 mt-6 border-2 border-dotted rounded-2xl w-fit py-1 px-3">
-          <a href="#" className="text-[#ffdb70] ">
+        <div className="flex gap-7 mt-6 border-2 border-solid border-[#ffdb70] rounded-2xl w-fit py-2 px-5">
+          <a
+            href="mailto:hey.arunkumar05@gmail.com"
+            className="text-[#ffdb70] hover:text-[#d2bb77] "
+          >
             <Mail size={20} />
           </a>
-          <a href="#" className="text-[#ffdb70]">
+          <a href="https://x.com/hiarun01" className="text-[#ffdb70]">
             <X size={20} />
           </a>
-          <a href="#" className="text-[#ffdb70]">
+          <a
+            href="https://www.linkedin.com/in/hiarun01/"
+            className="text-[#ffdb70] hover:text-[#d2bb77]"
+          >
             <Linkedin size={20} />
           </a>
-          <a href="#" className="text-[#ffdb70] ">
+          <a
+            href="https://github.com/hiarun01"
+            className="text-[#ffdb70] hover:text-[#d2bb77]"
+          >
             <Github size={20} />
           </a>
         </div>
