5de8f5e5d13fb7f4f66ddbd8507a8cba99b65f49
Itsarun01 <hey.arunkumar05@gmail.com>
2025-03-11 23:20:39 +0530

    update project des

--- src/Api/ProjectsApi.js
+++ src/Api/ProjectsApi.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "hi-rescue - (dev Stage)",
     durationAndInfo: "Personal Mega Project - 2025",
     description:
-      "AnimalSavior is a dedicated web platform that enables users to report animal-related incidents by submitting details like photos and location.Upon submission, the platform ensures that relevant volunteers in the area are promptly notified, allowing for quick action.",
+      "Hi-resuce is a web platform that makes animal rescue faster and easier. Users can report animal-related incidents by sharing photos, details, and exact locations. The platform instantly alerts nearby volunteers, helping them respond quickly. With real-time notifications and a network of dedicated rescuers, hi-rescue ensures that animals in need get timely help, improving rescue efforts and saving more lives",
     github: "https://github.com/hiarun01/hi-rescue",
     live: "https://hi-rescue.vercel.app/",
   },
