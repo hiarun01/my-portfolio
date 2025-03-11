7c55e94272a5ca050af36922eb39676425225790
Itsarun01 <hey.arunkumar05@gmail.com>
2025-03-11 23:08:43 +0530

    update project

--- src/Api/ProjectsApi.js
+++ src/Api/ProjectsApi.js
@@ -1,21 +1,11 @@
 export const projects = [
   {
     Id: "1",
-    title: "AnimalSavior - (dev Stage)",
+    title: "hi-rescue - (dev Stage)",
     durationAndInfo: "Personal Mega Project - 2025",
     description:
       "AnimalSavior is a dedicated web platform that enables users to report animal-related incidents by submitting details like photos and location.Upon submission, the platform ensures that relevant volunteers in the area are promptly notified, allowing for quick action.",
-    github: "https://github.com/hiarun01/AnimalSavior",
-    live: "https://animal-savior.vercel.app/",
-  },
-
-  {
-    Id: "3",
-    title: "Country Explorer",
-    durationAndInfo: "Personal Project - 2025",
-    description:
-      "Country Explorer is a web application that allows users to explore information about different countries around the world. The application is built using React and Vite, and it leverages various APIs to fetch and display country data, including details such as population, capital, languages, and more",
-    github: "https://github.com/hiarun01/Country-Explorer",
-    live: "https://country-explorer-kappa.vercel.app/",
+    github: "https://github.com/hiarun01/hi-rescue",
+    live: "https://hi-rescue.vercel.app/",
   },
 ];
