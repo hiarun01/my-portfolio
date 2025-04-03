70e8131b94cffe0f15a33d8bbe60151434ddfa8b
hiarun01 <hiarun02@gmail.com>
2025-04-03 18:54:28 +0530

    add favicon and change site tiitle

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -2,17 +2,24 @@ const Projects = () => {
   const projects = [
     {
       Id: "1",
-      title: "hi-rescue",
-      durationAndInfo: "Personal Mega Project - 2025",
+      title: "Hi-rescue",
       description:
-        "Hi-resuce is a web platform that makes animal rescue faster and easier. Users can report animal-related incidents by sharing photos, details, and exact locations. The platform instantly alerts nearby volunteers, helping them respond quickly. With real-time notifications and a network of dedicated rescuers, hi-rescue ensures that animals in need get timely help, improving rescue efforts and saving more lives",
+        "Hi-Rescue is a web platform that streamlines animal rescue. Users report incidents with photos, details, and locations, instantly alerting nearby volunteers. Real-time notifications ensure faster response, saving more animals.",
       github: "https://github.com/hiarun01/hi-rescue",
       live: "https://hi-rescue.vercel.app/",
     },
+    // {
+    //   Id: "2",
+    //   title: "Jobify",
+    //   description:
+    //     "Jobify is a web platform that makes animal rescue faster and easier. Users can report animal-related incidents by sharing photos, details, and exact locations. The platform instantly alerts nearby volunteers, helping them respond quickly. With real-time notifications and a network of dedicated rescuers, hi-rescue ensures that animals in need get timely help, improving rescue efforts and saving more lives",
+    //   github: "https://github.com/hiarun01/hi-rescue",
+    //   live: "https://hi-rescue.vercel.app/",
+    // },
   ];
   return (
     <div className="max-w-3xl mx-auto px-5">
-      <section className="mb-12">
+      <section className="mb-10">
         <h2 className="text-2xl font-bold text-[#fafafa] mb-6">
           Personal Projects
         </h2>
@@ -20,27 +27,26 @@ const Projects = () => {
           {projects.map((project, id) => {
             return (
               <div
-                className="border-l-2 rounded-2xl text-[#ffdb70] pl-4 "
+                className="border-l-2 rounded-4xl text-[#ffdb70] pl-4 "
                 key={id}
               >
                 <h3 className="text-lg font-semibold text-[#fafafa]">
                   {project.title}
                 </h3>
-                <p className="text-[#d6d6d6]">{project.durationAndInfo}</p>
-                <div className="mt-4 text-[#d6d6d6]">
+                <div className="mt-1 text-[#d6d6d6]">
                   <p className="mb-4">{project.description}</p>
                 </div>
 
-                <div className="mt-4 flex gap-4">
+                <div className="flex gap-5">
                   <a
                     href={project.github}
-                    className="text-[#ffdb70] hover:hover:text-[#d2bb77] flex items-center gap-1"
+                    className="text-[#ffdb70] hover:hover:text-[#d2bb77] flex items-center border-1 rounded-2xl px-4"
                   >
                     <span>View Code</span>
                   </a>
                   <a
                     href={project.live}
-                    className="text-[#ffdb70] hover:text-[#d2bb77] flex items-center gap-1"
+                    className="text-[#ffdb70] hover:text-[#d2bb77] flex items-center border-1 rounded-2xl px-4"
                   >
                     <span>Live Demo</span>
                   </a>
