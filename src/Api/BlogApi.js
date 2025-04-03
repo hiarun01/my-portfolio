eff6e6559be7f822c427ddfaee8333c0e48e9dfa
hiarun01 <hiarun02@gmail.com>
2025-04-03 17:15:46 +0530

    remove file

--- src/Api/BlogApi.js
+++
@@ -1,8 +0,0 @@
-export const blogPosts = [
-  {
-    title: "Let's Understand CI/CD Pipeline?",
-    date: "Feb 27, 2025",
-    readTime: "5 min read",
-    link: "https://hiarun.hashnode.dev/lets-understand-ci-cd-pipeline",
-  },
-];
