4d98f9297cb1758a23079c911e5ef2c7db34a762
Arun Kumar <89033797+hiarun01@users.noreply.github.com>
2025-05-23 21:56:09 +0530

    Update Projects.jsx

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,16 +1,7 @@
 const Projects = () => {
-  const clientProjects = [
-    {
-      Id: "1",
-      title: "Home intrio",
-      description:
-        "Designed and developed a fully responsive home interior portfolio website for an interior designer.",
-      live: "https://homeintrio.com/",
-    },
-  ];
   const personalProjects = [
     {
-      Id: "2",
+      Id: "1",
       title: "AI Image Enhancer",
       description:
         "AI Image Enhancer is a powerful, AI-driven web application that enhances and upscales your images with just one click.",
@@ -18,7 +9,7 @@ const Projects = () => {
       live: "https://hi-ai-image-enhancer.vercel.app/",
     },
     {
-      Id: "1",
+      Id: "2",
       title: "Jobify",
       description:
         "Jobify is a job portal where people can find and apply for jobs, and companies can post job openings. It’s built with a clean and user-friendly design, making the job search and hiring process easy for everyone.",
@@ -38,36 +29,6 @@ const Projects = () => {
 
   return (
     <div className="max-w-3xl mx-auto px-5">
-      {/* clients projects */}
-      <section className="mb-5">
-        <h2 className="text-2xl font-bold text-[#fafafa] mb-5 border-l-2 pl-2 rounded-3xl border-[#d2bb77]">
-          freelance Projects
-        </h2>
-        <div className="space-y-8">
-          {clientProjects.map((project, id) => {
-            return (
-              <div className="" key={id}>
-                <h3 className="text-lg font-semibold text-[#fafafa] border-l-2 pl-2 rounded-3xl border-[#d2bb77]">
-                  {project.title}
-                </h3>
-                <div className="mt-3 text-[#d6d6d6]">
-                  <p className="mb-4">{project.description}</p>
-                </div>
-
-                <div className="flex gap-5">
-                  <a
-                    href={project.live}
-                    className="text-[#ffdb70] hover:text-[#d2bb77] flex items-center border-1 rounded-2xl px-4"
-                  >
-                    <span>Live Demo</span>
-                  </a>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </section>
-
       {/* personal projects */}
       <section className="mb-10">
         <h2 className="text-2xl font-bold text-[#fafafa] mb-5 border-l-2 pl-2 rounded-3xl border-[#d2bb77]">
