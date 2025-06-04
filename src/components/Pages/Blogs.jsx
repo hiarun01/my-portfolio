e7e9a8e45b677c762fc2cdf18726a6f8267b1344
hiarun01 <hiarun02@gmail.com>
2025-06-05 00:17:35 +0530

    add new article

--- src/components/Pages/Blogs.jsx
+++ src/components/Pages/Blogs.jsx
@@ -3,6 +3,12 @@ import {motion} from "framer-motion";
 const Blogs = () => {
   const blogPosts = [
     {
+      title: "Redux vs Zustand: Which State Management Tool is the GOAT ?",
+      date: "Jun 5, 2025",
+      readTime: "3 min read",
+      link: "https://hiarun.hashnode.dev/redux-vs-zustand-which-state-management-tool-is-the-goat",
+    },
+    {
       title: "Top 5 VS Code Extensions for Frontend Developers",
       date: "May 26, 2025",
       readTime: "5 min read",
