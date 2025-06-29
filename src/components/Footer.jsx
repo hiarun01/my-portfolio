d0ffc19d32a3095238b036115b03ab82045df09c
hiarun01 <hiarun02@gmail.com>
2025-06-29 11:33:59 +0530

    made minor chnage in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ const Footer = () => {
     <>
       <footer className="max-w-3xl mx-auto py-5 border-t-2 border-[#ffdb70]">
         <p className="text-center text-[#fafafa]">
-          <strong>© {new Date().getFullYear()} hiarun01</strong>
+          <strong>
+            © {new Date().getFullYear()}{" "}
+            <a href="https://x.com/hiarun01" target="_black">
+              hiarun01
+            </a>{" "}
+          </strong>
         </p>
       </footer>
     </>
