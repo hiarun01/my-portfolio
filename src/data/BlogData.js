3701ae7a0124110e9a77bcd9e1bb23e8c6492d46
hiarun01 <hiarun02@gmail.com>
2025-07-18 11:17:27 +0530

    fixed blog post url

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,6 +3,6 @@ export const blogPosts = [
     title: "Redux vs Zustand: Which State Management Tool is the GOAT ?",
     date: "Jun 5, 2025",
     readTime: "3 min read",
-    link: "https://hiarun.hashnode.dev/redux-vs-zustand-which-state-management-tool-is-the-goat",
+    link: "https://medium.com/@hiarun01/redux-vs-zustand-which-state-management-tool-is-the-goat-6686573bca26",
   },
 ];
