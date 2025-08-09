06a643cf2a387bbd886adb246aceb8e1d93bb69a
hiarun01 <hiarun02@gmail.com>
2025-08-09 22:32:47 +0530

    fixed skills array item order

--- components/Data/data.ts
+++ components/Data/data.ts
@@ -1,4 +1,3 @@
-import {BsFiletypeSql} from "react-icons/bs";
 import {DiJavascript} from "react-icons/di";
 import {FaDocker, FaGitAlt, FaGithub} from "react-icons/fa";
 import {IoLogoNodejs} from "react-icons/io";
@@ -11,39 +10,31 @@ import {
 } from "react-icons/ri";
 import {
   SiExpress,
-  SiGreensock,
   SiMongodb,
   SiPostman,
-  SiPrisma,
   SiReactquery,
-  SiRedis,
   SiShadcnui,
-  SiVercel,
 } from "react-icons/si";
-import {
-  TbBrandCpp,
-  TbBrandFramerMotion,
-  TbBrandTypescript,
-} from "react-icons/tb";
+import {TbBrandFramerMotion, TbBrandTypescript} from "react-icons/tb";
 
 const skills = [
   {id: 1, icon: RiReactjsLine, text: "ReactJs"},
   {id: 2, icon: RiNextjsLine, text: "NextJs"},
-  {id: 4, icon: DiJavascript, text: "JavaScript"},
-  {id: 5, icon: TbBrandTypescript, text: "TypeScript"},
-  {id: 6, icon: RiTailwindCssFill, text: "Tailwind"},
-  {id: 7, icon: RiBearSmileLine, text: "Zustand"},
-  {id: 8, icon: SiReactquery, text: "Tanstack Query"},
-  {id: 9, icon: SiShadcnui, text: "shadcn"},
-  {id: 10, icon: TbBrandFramerMotion, text: "Motion"},
-  {id: 12, icon: IoLogoNodejs, text: "NodeJS"},
-  {id: 13, icon: SiExpress, text: "ExpressJS"},
-  {id: 15, icon: SiMongodb, text: "MongoDB"},
-  {id: 18, icon: SiPostman, text: "Postman"},
-  {id: 19, icon: FaGitAlt, text: "Git"},
-  {id: 20, icon: FaGithub, text: "GitHub"},
-  {id: 26, icon: FaDocker, text: "Docker"},
-  {id: 29, icon: PiFigmaLogoBold, text: "Figma"},
+  {id: 3, icon: DiJavascript, text: "JavaScript"},
+  {id: 4, icon: TbBrandTypescript, text: "TypeScript"},
+  {id: 5, icon: RiTailwindCssFill, text: "Tailwind"},
+  {id: 6, icon: RiBearSmileLine, text: "Zustand"},
+  {id: 7, icon: SiReactquery, text: "Tanstack Query"},
+  {id: 8, icon: SiShadcnui, text: "shadcn"},
+  {id: 9, icon: TbBrandFramerMotion, text: "Motion"},
+  {id: 10, icon: IoLogoNodejs, text: "NodeJS"},
+  {id: 11, icon: SiExpress, text: "ExpressJS"},
+  {id: 12, icon: SiMongodb, text: "MongoDB"},
+  {id: 13, icon: SiPostman, text: "Postman"},
+  {id: 14, icon: FaGitAlt, text: "Git"},
+  {id: 15, icon: FaGithub, text: "GitHub"},
+  {id: 16, icon: FaDocker, text: "Docker"},
+  {id: 17, icon: PiFigmaLogoBold, text: "Figma"},
 ];
 
 export {skills};
