3193003f4d955f738d5fb730a3fda9c37c9f4046
hiarun02 <hiarun02@gmail.com>
2025-12-10 21:18:01 +0530

    add and seperate blog data in new file

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,24 +1,5 @@
 import {FiCalendar, FiClock} from "react-icons/fi";
-
-type blogPosts = {
-  title: string;
-  date: string;
-  readTime: string;
-  link: string;
-  excerpt?: string;
-  tags?: string[];
-};
-
-export const blogPosts: blogPosts[] = [
-  {
-    title: "Redux vs Zustand: Which State Management Tool is the GOAT ?",
-    date: "Jun 5, 2025",
-    readTime: "3 min read",
-    link: "https://medium.com/@hiarun01/redux-vs-zustand-which-state-management-tool-is-the-goat-6686573bca26",
-    excerpt:
-      "A comprehensive comparison between Redux and Zustand for React state management, exploring their pros, cons, and use cases.",
-  },
-];
+import {blogPosts} from "./Data/BlogsData";
 
 const Blogs = () => {
   return (
