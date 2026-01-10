5f80aec3320897a6cae0103abda4717ce22267be
hiarun02 <hiarun02@gmail.com>
2026-01-10 15:45:19 +0530

    add logo images hackathon

--- components/Data/HackathonData.ts
+++ components/Data/HackathonData.ts
@@ -11,20 +11,20 @@ export const hackathons: Hackathon[] = [
     description:
       "i built KiroCore is a universal AI agent platform that transforms a single codebase into multiple specialized applications through configuration files.",
     date: "Oct 31st - Dec 6th, 2025",
-    logo: "K",
+    logo: "/kiro.png",
   },
   {
     title: "Code with Kiro Hackathon",
     description:
       "Built Rapid Report platform that lets people quickly and securely report emergencies. It ensures fast response, protects user privacy, and offers full anonymity whenever needed",
     date: "Aug 1rd - Sep 15th, 2025",
-    logo: "K",
+    logo: "/kiro.png",
   },
   {
     title: "Adobe Express Add-ons Hackathon",
     description:
       "Developed a color palette generator for Adobe Express. It helps users quickly generate beautiful, ready-to-use color palettes based on user Prompt.",
     date: "May 19rd - Jul 21th, 2025",
-    logo: "A",
+    logo: "/adobe.jpg",
   },
 ];
